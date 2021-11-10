98607f6af7159cfa208c80cc756e43c3b27b30af
John Efren Pila <jefren.pila2@gmail.com>
2021-11-11 01:13:08 +0800

    admin

--- teknotrade_app/urls.py
+++ teknotrade_app/urls.py
@@ -20,7 +20,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+    path('admin/', include('admin_app.urls', namespace='admin_app')),
     path('admin/', admin.site.urls),
     path('', include('user_app.urls', namespace='user_app')),
-    path('admin/', include('admin_app.urls', namespace='admin_app'))
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
