ce12f09b9d55b2b22b0135bedfb97a877c049495
fmarie-it <f.marieit@gmail.com>
2021-11-11 00:32:09 +0800

    Fix CRUD register

--- user_app/models.py
+++ user_app/models.py
@@ -4,11 +4,11 @@ from django.utils import timezone
 
 # Create your models here.
 
-class CustomUser(models.Model):
-    user_id = models.OneToOneField(User, on_delete=models.SET_NULL, null=True, blank=True)
-    is_admin = models.BooleanField(default=False)
-    # created_at = models.DateTimeField(default = timezone.now)
-    # updated_at = models.DateTimeField(auto_now=True)
+# class CustomUser(models.Model):
+#     user_id = models.OneToOneField(User, on_delete=models.SET_NULL, null=True, blank=True)
+#     is_admin = models.BooleanField(default=False)
+#     # created_at = models.DateTimeField(default = timezone.now)
+#     # updated_at = models.DateTimeField(auto_now=True)
     
-    class Meta:
-        db_table = "CustomUser"
+#     class Meta:
+#         db_table = "CustomUser"
