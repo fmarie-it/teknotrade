2fbc5f30cb11a5f067058b6a6d394ee3f258005d
Supermaneman <emanuel.rabe18@gmail.com>
2021-11-16 00:58:35 +0800

    back

--- user_app/views.py
+++ user_app/views.py
@@ -6,23 +6,23 @@ from django.contrib.auth import authenticate, login, logout
 # from .models import CustomUser
 from .forms import UserForm
 from django.contrib.auth.decorators import login_required
-import pyrebase
+#import pyrebase
 
 # Create your views here.
-config = {
-    'apiKey': "AIzaSyCMEEQ4rALIOf5lX0YuCvXXFABPk1ZvLjg",
-    'authDomain': "databasedj-45b67.firebaseapp.com",
-    'databaseURL': "https://databasedj-45b67-default-rtdb.firebaseio.com",
-    'projectId': "databasedj-45b67",
-    'storageBucket': "databasedj-45b67.appspot.com",
-    'messagingSenderId': "606108675515",
-    'appId': "1:606108675515:web:df26139a23b59f7f03e89e",
+#config = {
+#    'apiKey': "AIzaSyCMEEQ4rALIOf5lX0YuCvXXFABPk1ZvLjg",
+#    'authDomain': "databasedj-45b67.firebaseapp.com",
+#    'databaseURL': "https://databasedj-45b67-default-rtdb.firebaseio.com",
+#    'projectId': "databasedj-45b67",
+#    'storageBucket': "databasedj-45b67.appspot.com",
+#    'messagingSenderId': "606108675515",
+#    'appId': "1:606108675515:web:df26139a23b59f7f03e89e",
  
-}
+#}
 
-firebase=pyrebase.initialize_app(config)
-authe = firebase.auth()
-database = firebase.database()
+#firebase=pyrebase.initialize_app(config)
+#authe = firebase.auth()
+#database = firebase.database()
 
 
 class LoginView(View):
